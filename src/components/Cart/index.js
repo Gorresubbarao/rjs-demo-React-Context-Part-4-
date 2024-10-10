5a321adef6b3d6dacf16302c2d59144c4dc62768
Gorresubbarao <gsubbu2109@gmail.com>
2024-10-10 14:32:19 +0000

    demo

---
+++ src/components/Cart/index.js
@@ -0,0 +1,34 @@
+import CartContext from '../../context/CartContext'
+
+import Header from '../Header'
+import CartListView from '../CartListView'
+import EmptyCartView from '../EmptyCartView'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+
+      const showEmptyCartView = cartList.length === 0
+      // console.log('showEmptyCartView', showEmptyCartView)
+      return (
+        <>
+          <Header />
+          {showEmptyCartView ? (
+            <EmptyCartView />
+          ) : (
+            <div className="cart-container">
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <CartListView />
+              </div>
+            </div>
+          )}
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
